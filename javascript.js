2e09788c740d0429984d9f13cf0748095bae147e
xRedril <nathanielhagans@gmail.com>
2022-06-20 13:41:02 -0600

    Interactive UI for RPS

--- javascript.js
+++ javascript.js
@@ -1,73 +1,143 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+const playerChoice = document.querySelector('.player-choice');
+const computerChoice = document.querySelector('.computer-choice');
+const chooseFighter = document.querySelector('.choose-fighter');
+const explanation = document.querySelector('.first-to-five');
+const modal = document.querySelector('#myModal');
+const btn = document.querySelector('#myBtn');
+const restartBtn = document.querySelector('.restart');
+const playAgain = document.querySelector('.play-again');
 
 function computerPlay() {
     return choices[Math.floor(Math.random() * 3)]
 }
 
+function youWin() {
+    win++
+    playerScore.textContent = `Player: ${win}`
+    playerChoice.textContent = `${playerSelection}`
+    computerChoice.textContent = `${computerSelection}`
+    chooseFighter.textContent = `You Win!`
+    explanation.textContent = `${playerSelection} beats ${computerSelection}`
+}
+
+function youLose() {
+    loss++
+    computerScore.textContent = `Computer: ${loss}`
+    playerChoice.textContent = `${playerSelection}`
+    computerChoice.textContent = `${computerSelection}`
+    chooseFighter.textContent = `You Lose!`
+    explanation.textContent = `${computerSelection} beats ${playerSelection}`
+}
+
+function youTie() {
+    tie++
+    chooseFighter.textContent = `You Tied!`
+    playerChoice.textContent = `${playerSelection}`
+    computerChoice.textContent = `${computerSelection}`
+    explanation.textContent = `${playerSelection} ties with ${computerSelection}`
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'Rock') {
         if (computerSelection === 'Scissors') {
-            console.log(`You Win! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 0;
+            youWin()
         } else if (computerSelection === 'Paper') {
-            console.log(`You Lost! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 1;
+            youLose()
         } else {
-            console.log(`You Tied! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 2;
+            youTie()
         }
     } else if (playerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
-            console.log(`You Win! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 0;
+            youWin()
         } else if (computerSelection === 'Scissors') {
-            console.log(`You Lost! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 1;
+            youLose()
         } else {
-            console.log(`You Tied! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 2;
+            youTie()
         }
     } else {
         if (computerSelection === 'Paper') {
-            console.log(`You Win! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 0;
+            youWin()
         } else if (computerSelection === 'Rock') {
-            console.log(`You Lost! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 1;
+            youLose()
         } else {
-            console.log(`You Tied! Player one picked ${playerSelection} and Player two picked ${computerSelection}.`)
-            playerWins = 2;
+            youTie()
         }
     }
 }
+
 
 let playerSelection = "Rock";
 let computerSelection = computerPlay();
-let playerWins = 0;
-
-console.log(playRound(playerSelection, computerSelection));
 
 let total = 0;
-let wins = 0;
+let win = 0;
 let loss = 0;
 let tie = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Choose Rock, Paper, or Scissors: ", "");
-        computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        if (playerWins === 0) {
-            total = total + 1;
-            wins += 1
-        } else if (playerWins === 1) {
-            total = total + 1;
-            loss += 1;
-        } else {
-            total = total + 1;
-            tie += 1;
-        }
+function isGameOver() {
+    return win === 5 || loss === 5
+}
 
-        console.log(`Total: ${total}, Wins: ${wins}, Losses: ${loss}, Ties: ${tie}`)
+function openModal() {
+    modal.style.display = 'block';
+    if (win === 5) {
+        playAgain.textContent = `You win! Would you like to play again?`
+    } else if (loss === 5) {
+        playAgain.textContent = `You lose! Better luck next time.`
     }
 }
+
+function rockFighter(e) {
+    if (isGameOver()) {
+        return openModal()
+    }
+
+    playerSelection = 'Rock'
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+
+}
+
+rockBtn.addEventListener('click', rockFighter);
+
+function paperFighter(e) {
+    if (isGameOver()) {
+        return openModal()
+    }
+    playerSelection = 'Paper';
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+}
+
+paperBtn.addEventListener('click', paperFighter);
+
+function scissorsFighter(e) {
+    if (isGameOver()) {
+        return openModal()
+    }
+    playerSelection = 'Scissors';
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+}
+
+scissorsBtn.addEventListener('click', scissorsFighter);
+
+function restartGame() {
+    win = 0;
+    loss = 0;
+    playerScore.textContent = `Player: ${win}`
+    computerScore.textContent = `Computer: ${loss}`
+    playerChoice.textContent = `❔`
+    computerChoice.textContent = `❔`
+    chooseFighter.textContent = `Choose your fighter!`
+    explanation.textContent = `First to 5 points wins the game.`
+    modal.style.display = 'none';
+}
+
+restartBtn.addEventListener('click', restartGame);
